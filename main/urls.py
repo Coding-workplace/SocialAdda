0d8fa660a36db6451918dc0fa4c1e6fce942c968
darshil <119782708+Darshil-Solanki@users.noreply.github.com>
2023-02-05 20:36:47 +0530

    Added login,logout view

---
+++ main/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+
+urlpatterns = [
+    path("", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("signup", views.signup_view, name="signup"),
+    path("<str:username>", views.profile_view, name='profile'),
+]
+
--- main/views.py
+++ main/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
+from .models import *
 # Create your views here.
+
+def profile_view(request):
+	pass
+
+def signup_view(request):
+	pass
+
+def login_view(request):
+	'''login'''
+	if request.method=='POST':
+		username=request.POST.get('username')
+		password=request.POST.get('password')
+		user=authenticate(username=username,password=password)
+		if user is not None:
+			login(request,user)
+			messages.success(request,"Login Successfully!")
+			return redirect("home_page")
+		else:
+			messages.error(request,"Invalid Credentials!")
+	else:
+		return HttpResponse("Get request")
+
+def logout_view(request):
+    logout(request)
+	messages.success(request,"Logout Successfully!")
+    return redirect("/")
