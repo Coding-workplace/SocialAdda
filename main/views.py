a8037eb8a9e486d0b4789bee6da2b6a2ef13e8fa
darshil <119782708+Darshil-Solanki@users.noreply.github.com>
2023-02-09 19:04:59 +0530

    Added functionality

--- main/views.py
+++ main/views.py
@@ -29,37 +29,36 @@ def home_view(request):
 		return redirect("login")
 
 
-def profile_view(request,username):	
-	pass
-	# user = User.objects.get(username=username)
-    # all_posts = Post.objects.filter(creater=user).order_by('-date_created')
-    # paginator = Paginator(all_posts, 10)
-    # page_number = request.GET.get('page')
-    # if page_number == None:
-    #     page_number = 1
-    # posts = paginator.get_page(page_number)
-    # followings = []
-    # suggestions = []
-    # follower = False
-    # if request.user.is_authenticated:
-    #     followings = Follower.objects.filter(followers=request.user).values_list('user', flat=True)
-    #     suggestions = User.objects.exclude(pk__in=followings).exclude(username=request.user.username).order_by("?")[:6]
+def profile_view(request,username):
+    user = User.objects.get(username=username)
+    all_posts = Post.objects.filter(creater=user).order_by('-date_created')
+    paginator = Paginator(all_posts, 10)
+    page_number = request.GET.get('page')
+    if page_number == None:
+        page_number = 1
+    posts = paginator.get_page(page_number)
+    followings = []
+    suggestions = []
+    follower = False
+    if request.user.is_authenticated:
+        followings = Follower.objects.filter(followers=request.user).values_list('user', flat=True)
+        suggestions = User.objects.exclude(pk__in=followings).exclude(username=request.user.username).order_by("?")[:6]
 
-    #     if request.user in Follower.objects.get(user=user).followers.all():
-    #         follower = True
+        if request.user in Follower.objects.get(user=user).followers.all():
+            follower = True
     
-    # follower_count = Follower.objects.get(user=user).followers.all().count()
-    # following_count = Follower.objects.filter(followers=user).count()
-    # return render(request, 'network/profile.html', {
-    #     "username": user,
-    #     "posts": posts,
-    #     "posts_count": all_posts.count(),
-    #     "suggestions": suggestions,
-    #     "page": "profile",
-    #     "is_follower": follower,
-    #     "follower_count": follower_count,
-    #     "following_count": following_count
-    # })
+    follower_count = Follower.objects.get(user=user).followers.all().count()
+    following_count = Follower.objects.filter(followers=user).count()
+    return render(request, 'profile.html', {
+        "username": user,
+        "posts": posts,
+        "posts_count": all_posts.count(),
+        "suggestions": suggestions,
+        "page": "profile",
+        "is_follower": follower,
+        "follower_count": follower_count,
+        "following_count": following_count
+    })
 
 def signup_view(request):
 	if request.method == "POST":
@@ -124,45 +123,43 @@ def logout_view(request):
 
 
 def following(request):
-    pass
-#     if request.user.is_authenticated:
-#         following_user = Follower.objects.filter(followers=request.user).values('user')
-#         all_posts = Post.objects.filter(creater__in=following_user).order_by('-date_created')
-#         paginator = Paginator(all_posts, 10)
-#         page_number = request.GET.get('page')
-#         if page_number == None:
-#             page_number = 1
-#         posts = paginator.get_page(page_number)
-#         followings = Follower.objects.filter(followers=request.user).values_list('user', flat=True)
-#         suggestions = User.objects.exclude(pk__in=followings).exclude(username=request.user.username).order_by("?")[:6]
-#         return render(request, "network/index.html", {
-#             "posts": posts,
-#             "suggestions": suggestions,
-#             "page": "following"
-#         })
-#     else:
-#         return HttpResponseRedirect(reverse('login'))
+    if request.user.is_authenticated:
+        following_user = Follower.objects.filter(followers=request.user).values('user')
+        all_posts = Post.objects.filter(creater__in=following_user).order_by('-date_created')
+        paginator = Paginator(all_posts, 10)
+        page_number = request.GET.get('page')
+        if page_number == None:
+            page_number = 1
+        posts = paginator.get_page(page_number)
+        followings = Follower.objects.filter(followers=request.user).values_list('user', flat=True)
+        suggestions = User.objects.exclude(pk__in=followings).exclude(username=request.user.username).order_by("?")[:6]
+        return render(request, "feed.html", {
+            "posts": posts,
+            "suggestions": suggestions,
+            "page": "following"
+        })
+    else:
+        return redirect('login')
 
 def saved(request):
-    pass
-#     if request.user.is_authenticated:
-#         all_posts = Post.objects.filter(savers=request.user).order_by('-date_created')
+    if request.user.is_authenticated:
+        all_posts = Post.objects.filter(savers=request.user).order_by('-date_created')
 
-#         paginator = Paginator(all_posts, 10)
-#         page_number = request.GET.get('page')
-#         if page_number == None:
-#             page_number = 1
-#         posts = paginator.get_page(page_number)
+        paginator = Paginator(all_posts, 10)
+        page_number = request.GET.get('page')
+        if page_number == None:
+            page_number = 1
+        posts = paginator.get_page(page_number)
 
-#         followings = Follower.objects.filter(followers=request.user).values_list('user', flat=True)
-#         suggestions = User.objects.exclude(pk__in=followings).exclude(username=request.user.username).order_by("?")[:6]
-#         return render(request, "network/index.html", {
-#             "posts": posts,
-#             "suggestions": suggestions,
-#             "page": "saved"
-#         })
-#     else:
-#         return HttpResponseRedirect(reverse('login'))
+        followings = Follower.objects.filter(followers=request.user).values_list('user', flat=True)
+        suggestions = User.objects.exclude(pk__in=followings).exclude(username=request.user.username).order_by("?")[:6]
+        return render(request, "feed.html", {
+            "posts": posts,
+            "suggestions": suggestions,
+            "page": "saved"
+        })
+    else:
+        return redirect('login')
         
 
 
@@ -183,178 +180,171 @@ def create_post(request):
 @csrf_exempt
 def edit_post(request, post_id):
     pass
-    # if request.method == 'POST':
-    #     text = request.POST.get('text')
-    #     pic = request.FILES.get('picture')
-    #     img_chg = request.POST.get('img_change')
-    #     post_id = request.POST.get('id')
-    #     post = Post.objects.get(id=post_id)
-    #     try:
-    #         post.content_text = text
-    #         if img_chg != 'false':
-    #             post.content_image = pic
-    #         post.save()
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        pic = request.FILES.get('picture')
+        img_chg = request.POST.get('img_change')
+        post_id = request.POST.get('id')
+        post = Post.objects.get(id=post_id)
+        try:
+            post.content_text = text
+            if img_chg != 'false':
+                post.content_image = pic
+            post.save()
             
-    #         if(post.content_text):
-    #             post_text = post.content_text
-    #         else:
-    #             post_text = False
-    #         if(post.content_image):
-    #             post_image = post.img_url()
-    #         else:
-    #             post_image = False
+            if(post.content_text):
+                post_text = post.content_text
+            else:
+                post_text = False
+            if(post.content_image):
+                post_image = post.img_url()
+            else:
+                post_image = False
             
-    #         return JsonResponse({
-    #             "success": True,
-    #             "text": post_text,
-    #             "picture": post_image
-    #         })
-    #     except Exception as e:
-    #         print('-----------------------------------------------')
-    #         print(e)
-    #         print('-----------------------------------------------')
-    #         return JsonResponse({
-    #             "success": False
-    #         })
-    # else:
-    #         return HttpResponse("Method must be 'POST'")
+            return JsonResponse({
+                "success": True,
+                "text": post_text,
+                "picture": post_image
+            })
+        except Exception as e:
+            print('-----------------------------------------------')
+            print(e)
+            print('-----------------------------------------------')
+            return JsonResponse({
+                "success": False
+            })
+    else:
+            return HttpResponse("Method must be 'POST'")
 
 @csrf_exempt
 def like_post(request, id):
-    pass
-    # if request.user.is_authenticated:
-    #     if request.method == 'PUT':
-    #         post = Post.objects.get(pk=id)
-    #         print(post)
-    #         try:
-    #             post.likers.add(request.user)
-    #             post.save()
-    #             return HttpResponse(status=204)
-    #         except Exception as e:
-    #             return HttpResponse(e)
-    #     else:
-    #         return HttpResponse("Method must be 'PUT'")
-    # else:
-    #     return HttpResponseRedirect(reverse('login'))
+    if request.user.is_authenticated:
+        if request.method == 'PUT':
+            post = Post.objects.get(pk=id)
+            print(post)
+            try:
+                post.likers.add(request.user)
+                post.save()
+                return HttpResponse(status=204)
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse("Method must be 'PUT'")
+    else:
+        return redirect('login')
 
 @csrf_exempt
 def unlike_post(request, id):
-    pass
-    # if request.user.is_authenticated:
-    #     if request.method == 'PUT':
-    #         post = Post.objects.get(pk=id)
-    #         print(post)
-    #         try:
-    #             post.likers.remove(request.user)
-    #             post.save()
-    #             return HttpResponse(status=204)
-    #         except Exception as e:
-    #             return HttpResponse(e)
-    #     else:
-    #         return HttpResponse("Method must be 'PUT'")
-    # else:
-    #     return HttpResponseRedirect(reverse('login'))
+    if request.user.is_authenticated:
+        if request.method == 'PUT':
+            post = Post.objects.get(pk=id)
+            print(post)
+            try:
+                post.likers.remove(request.user)
+                post.save()
+                return HttpResponse(status=204)
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse("Method must be 'PUT'")
+    else:
+        return redirect('login')
 
 @csrf_exempt
 def save_post(request, id):
-    pass
-    # if request.user.is_authenticated:
-    #     if request.method == 'PUT':
-    #         post = Post.objects.get(pk=id)
-    #         print(post)
-    #         try:
-    #             post.savers.add(request.user)
-    #             post.save()
-    #             return HttpResponse(status=204)
-    #         except Exception as e:
-    #             return HttpResponse(e)
-    #     else:
-    #         return HttpResponse("Method must be 'PUT'")
-    # else:
-    #     return HttpResponseRedirect(reverse('login'))
+    if request.user.is_authenticated:
+        if request.method == 'PUT':
+            post = Post.objects.get(pk=id)
+            print(post)
+            try:
+                post.savers.add(request.user)
+                post.save()
+                return HttpResponse(status=204)
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse("Method must be 'PUT'")
+    else:
+        return redirect('login')
 
 @csrf_exempt
 def unsave_post(request, id):
-    pass
-    # if request.user.is_authenticated:
-    #     if request.method == 'PUT':
-    #         post = Post.objects.get(pk=id)
-    #         print(post)
-    #         try:
-    #             post.savers.remove(request.user)
-    #             post.save()
-    #             return HttpResponse(status=204)
-    #         except Exception as e:
-    #             return HttpResponse(e)
-    #     else:
-    #         return HttpResponse("Method must be 'PUT'")
-    # else:
-    #     return HttpResponseRedirect(reverse('login'))
+    if request.user.is_authenticated:
+        if request.method == 'PUT':
+            post = Post.objects.get(pk=id)
+            print(post)
+            try:
+                post.savers.remove(request.user)
+                post.save()
+                return HttpResponse(status=204)
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse("Method must be 'PUT'")
+    else:
+        return redirect('login')
 
 @csrf_exempt
 def follow(request, username):
-    pass
-    # if request.user.is_authenticated:
-    #     if request.method == 'PUT':
-    #         user = User.objects.get(username=username)
-    #         print(f".....................User: {user}......................")
-    #         print(f".....................Follower: {request.user}......................")
-    #         try:
-    #             (follower, create) = Follower.objects.get_or_create(user=user)
-    #             follower.followers.add(request.user)
-    #             follower.save()
-    #             return HttpResponse(status=204)
-    #         except Exception as e:
-    #             return HttpResponse(e)
-    #     else:
-    #         return HttpResponse("Method must be 'PUT'")
-    # else:
-    #     return HttpResponseRedirect(reverse('login'))
+    if request.user.is_authenticated:
+        if request.method == 'PUT':
+            user = User.objects.get(username=username)
+            print(f".....................User: {user}......................")
+            print(f".....................Follower: {request.user}......................")
+            try:
+                (follower, create) = Follower.objects.get_or_create(user=user)
+                follower.followers.add(request.user)
+                follower.save()
+                return HttpResponse(status=204)
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse("Method must be 'PUT'")
+    else:
+        return redirect('login')
 
 @csrf_exempt
 def unfollow(request, username):
-    pass
-    # if request.user.is_authenticated:
-    #     if request.method == 'PUT':
-    #         user = User.objects.get(username=username)
-    #         print(f".....................User: {user}......................")
-    #         print(f".....................Unfollower: {request.user}......................")
-    #         try:
-    #             follower = Follower.objects.get(user=user)
-    #             follower.followers.remove(request.user)
-    #             follower.save()
-    #             return HttpResponse(status=204)
-    #         except Exception as e:
-    #             return HttpResponse(e)
-    #     else:
-    #         return HttpResponse("Method must be 'PUT'")
-    # else:
-    #     return HttpResponseRedirect(reverse('login'))
+    if request.user.is_authenticated:
+        if request.method == 'PUT':
+            user = User.objects.get(username=username)
+            print(f".....................User: {user}......................")
+            print(f".....................Unfollower: {request.user}......................")
+            try:
+                follower = Follower.objects.get(user=user)
+                follower.followers.remove(request.user)
+                follower.save()
+                return HttpResponse(status=204)
+            except Exception as e:
+                return HttpResponse(e)
+        else:
+            return HttpResponse("Method must be 'PUT'")
+    else:
+        return redirect('login')
 
 
 @csrf_exempt
 def comment(request, post_id):
-    pass
-    # if request.user.is_authenticated:
-    #     if request.method == 'POST':
-    #         data = json.loads(request.body)
-    #         comment = data.get('comment_text')
-    #         post = Post.objects.get(id=post_id)
-    #         try:
-    #             newcomment = Comment.objects.create(post=post,commenter=request.user,comment_content=comment)
-    #             post.comment_count += 1
-    #             post.save()
-    #             print(newcomment.serialize())
-    #             return JsonResponse([newcomment.serialize()], safe=False, status=201)
-    #         except Exception as e:
-    #             return HttpResponse(e)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            data = json.loads(request.body)
+            comment = data.get('comment_text')
+            post = Post.objects.get(id=post_id)
+            try:
+                newcomment = Comment.objects.create(post=post,commenter=request.user,comment_content=comment)
+                post.comment_count += 1
+                post.save()
+                print(newcomment.serialize())
+                return JsonResponse([newcomment.serialize()], safe=False, status=201)
+            except Exception as e:
+                return HttpResponse(e)
     
-    #     post = Post.objects.get(id=post_id)
-    #     comments = Comment.objects.filter(post=post)
-    #     comments = comments.order_by('-comment_time').all()
-    #     return JsonResponse([comment.serialize() for comment in comments], safe=False)
-    # else:
-    #     return HttpResponseRedirect(reverse('login'))
+        post = Post.objects.get(id=post_id)
+        comments = Comment.objects.filter(post=post)
+        comments = comments.order_by('-comment_time').all()
+        return JsonResponse([comment.serialize() for comment in comments], safe=False)
+    else:
+        return redirect('login')
 
 @csrf_exempt
 def delete_post(request, post_id):
@@ -372,4 +362,4 @@ def delete_post(request, post_id):
         else:
             return HttpResponse("Method must be 'PUT'")
     else:
-        return HttpResponseRedirect(reverse('login'))
+        return redirect('login')
